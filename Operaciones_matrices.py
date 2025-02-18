df4b99a78c05acb46834594ee6f4f34362a228d7
Keylor2002 <85804030+Keylor2002@users.noreply.github.com>
2025-02-18 10:53:10 -0600

    Update Operaciones_matrices.py

--- Operaciones_matrices.py
+++ Operaciones_matrices.py
@@ -125,18 +125,21 @@ def mostrar_explicacion_operacion(operacion):
             Matriz A:
             
             [ a11  a12 ]
+            
             [ a21  a22 ]
 
 
             Matriz B: 
             
             [ b11  b12 ]
+            
             [ b21  b22 ]
 
 
             Resultado de A + B:
             
             [ a11 + b11  a12 + b12 ]
+            
             [ a21 + b21  a22 + b22 ]
             
             """)
@@ -150,18 +153,21 @@ def mostrar_explicacion_operacion(operacion):
             Matriz A:
             
             [ a11  a12 ]
+            
             [ a21  a22 ]
 
 
             Matriz B:
 
             [ b11  b12 ]
+            
             [ b21  b22 ]
 
 
             Resultado de A - B:
             
             [ a11 - b11  a12 - b12 ]
+            
             [ a21 - b21  a22 - b22 ]
         """)
     elif operacion == "Multiplicación":
@@ -172,16 +178,19 @@ def mostrar_explicacion_operacion(operacion):
             Matriz A:
             
             [ a11  a12 ]
+            
             [ a21  a22 ]
 
             Matriz B:
             
             [ b11  b12 ]
+            
             [ b21  b22 ]
 
             Resultado de A * B:
             
             [ (a11*b11 + a12*b21)  (a11*b12 + a12*b22) ]
+            
             [ (a21*b11 + a22*b21)  (a21*b12 + a22*b22) ]
         """)
 
