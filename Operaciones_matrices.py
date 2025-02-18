6a334e73f66ae1c5115c16a8bd88a8dcfd570ae2
Keylor2002 <85804030+Keylor2002@users.noreply.github.com>
2025-02-18 10:50:01 -0600

    Update Operaciones_matrices.py

--- Operaciones_matrices.py
+++ Operaciones_matrices.py
@@ -123,18 +123,21 @@ def mostrar_explicacion_operacion(operacion):
             Se suman sus elementos posición por posición, como sigue:
 
             Matriz A:
+            
             [ a11  a12 ]
             
             [ a21  a22 ]
 
 
             Matriz B: 
+            
             [ b11  b12 ]
             
             [ b21  b22 ]
 
 
             Resultado de A + B:
+            
             [ a11 + b11  a12 + b12 ]
             
             [ a21 + b21  a22 + b22 ]
@@ -148,18 +151,21 @@ def mostrar_explicacion_operacion(operacion):
             Se restan sus elementos posición por posición, como sigue:
 
             Matriz A:
+            
             [ a11  a12 ]
             
             [ a21  a22 ]
 
 
             Matriz B:
+
             [ b11  b12 ]
             
             [ b21  b22 ]
 
 
             Resultado de A - B:
+            
             [ a11 - b11  a12 - b12 ]
             
             [ a21 - b21  a22 - b22 ]
@@ -170,16 +176,19 @@ def mostrar_explicacion_operacion(operacion):
             Para multiplicar matrices, el número de **columnas de la primera matriz** debe ser igual al número de **filas de la segunda matriz**.
 
             Matriz A:
+            
             [ a11  a12 ]
             
             [ a21  a22 ]
 
             Matriz B:
+            
             [ b11  b12 ]
             
             [ b21  b22 ]
 
             Resultado de A * B:
+            
             [ (a11*b11 + a12*b21)  (a11*b12 + a12*b22) ]
             
             [ (a21*b11 + a22*b21)  (a21*b12 + a22*b22) ]
